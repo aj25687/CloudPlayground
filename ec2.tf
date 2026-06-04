c9eef71894d29b4c6a38d477ee15b87589cd329f
aj25687 <javantika.jain@gmail.com>
2026-06-04 11:32:51 -0400

    adding notes

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,7 @@
 # 1. Public Web Server
 resource "aws_instance" "web_server" {
   ami = "ami-0c7217cdde317cfec" # Ubuntu 22.04 LTS in us-east-1 (example)
+  # ami is Amazon Machine Image -> basically, provides a template of an OS 
   instance_type = "t2.micro"
   
   subnet_id = aws_subnet.public.id
